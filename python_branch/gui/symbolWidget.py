5ece967dfd887a52019cbdb3c834c61df1161eb3
Markus Dittrich <haskelladdict@users.sourceforge.net>
2010-09-05 20:00:42 -0400

    Added synchronization object for symbol selector widgets.

--- python_branch/gui/symbolWidget.py
+++ python_branch/gui/symbolWidget.py
@@ -32,8 +32,12 @@ def add_symbols_to_widget(symbols, widget):
     Adds all passed knitting symbols to the tab widget.
     """
 
+    # the synchronizer object makes sure one object is selected
+    # at a time and keeps track of which one
+    synchronizer = Synchronizer()
+
     symbolsByCategory = sort_symbols_by_category(symbols)
-    for (symbolCategory, symbols) in symbolsByCategory.items():
+    for (symbolCategory, symbols) in symbolsByCategory:
 
         # layout for current tab
         tab    = QWidget()
@@ -49,7 +53,7 @@ def add_symbols_to_widget(symbols, widget):
         # add them to the tab
         for (row, symbolEntry) in enumerate(rawList):
             symbol = symbolEntry[1]
-            newItem = SymbolSelectorItem(symbol)
+            newItem = SymbolSelectorItem(symbol, synchronizer)
             layout.addWidget(newItem, row, 0)
             layout.addWidget(QLabel(symbol["patternName"]), row, 1)
 
@@ -58,67 +62,142 @@ def add_symbols_to_widget(symbols, widget):
         scrollArea.setWidget(tab)
         widget.addTab(scrollArea, symbolCategory)
 
+        if symbolCategory == "basic":
+            widget.setCurrentWidget(scrollArea)
+
 
 
 def sort_symbols_by_category(symbols):
     """
-    Given a dictionary of knitting symbols returns a dictionary
-    with key category and value a list of corresponding symbols
+    Given a dictionary of knitting symbols returns a list with
+    tuples of category names and list of corresponding symbols.
+    The category names are sorted with basic coming first.
     """
 
-    sortedSymbols = {}
+    # assemble the categories
+    rawSortedSymbols = {}
     for symbol in symbols.values():
        
         symbolKey = symbol["category"]
-        if symbolKey in sortedSymbols:
-            sortedSymbols[symbolKey].append(symbol)
+        if symbolKey in rawSortedSymbols:
+            rawSortedSymbols[symbolKey].append(symbol)
         else:
-            sortedSymbols[symbolKey] = [symbol]
+            rawSortedSymbols[symbolKey] = [symbol]
 
+    # sort them
+    sortedSymbols = []
+    for (key, value) in rawSortedSymbols.items():
+        sortedSymbols.append((key,value))
 
+    sortedSymbols.sort(lambda x,y: cmp(x[0],y[0]))
+    
     return sortedSymbols
 
 
 
 
+#########################################################
+## 
+## class for managing a single symbol selector item
+##
+#########################################################
 class SymbolSelectorItem(QFrame):
 
-    def __init__(self, symbol, parent = None):
+    def __init__(self, symbol, synchronizer, parent = None):
 
         QFrame.__init__(self, parent)
-        self.__name = symbol["patternName"]
+        self.name = symbol["patternName"]
+        self.__synchronizer = synchronizer
 
-       
-        
-        selectedStyleSheet = "border-width: 1px;" \
-                             "border-style: solid;" \
-                             "border-color: red;" \
-                             "background-color: lightblue;"
+        # define and set stylesheets
+        self.define_stylesheets() 
+        self.setStyleSheet(self.__unselectedStyleSheet)
 
-        unselectedStyleSheet = "border-width: 1px;" \
-                               "border-style: solid;" \
-                               "border-color: black;" \
-                               "background-color: white;"
-
-        self.setStyleSheet(unselectedStyleSheet)
-
-
+        # add the symbol's svg
         svgWidget = QSvgWidget(symbol["svgPath"]) 
-
         svgWidth = symbol["patternWidth"].toInt()[0]
         svgWidget.setMaximumSize(QSize(svgWidth * 30, 30))
 
-
+        # finalize the layout
         layout    = QHBoxLayout()
         layout.setContentsMargins( 0, 0, 0, 0 )
         layout.addWidget(svgWidget)
-
         self.setLayout(layout)
 
 
 
+    def define_stylesheets(self):
+        """
+        Defines the stylesheets used for active/inactive look
+        of this widget.
+        """
+
+        self.__selectedStyleSheet = "border-width: 1px;" \
+                                    "border-style: solid;" \
+                                    "border-color: red;" \
+                                    "background-color: lightblue;"
+
+        self.__unselectedStyleSheet = "border-width: 1px;" \
+                                      "border-style: solid;" \
+                                      "border-color: black;" \
+                                      "background-color: white;"
+
+       
+    def mousePressEvent(self, event): 
+        """
+        Acts on mouse press events and uses the synchronizer
+        for selecting.
+        """
+
+        self.__synchronizer.select(self)
 
 
 
+    def activate_me(self):
+        """
+        This slot activates the item.
+        """
+
+        self.setStyleSheet(self.__selectedStyleSheet)
+
+
+
+    def inactivate_me(self):
+        """
+        This slot inactivates the item.
+        """
+
+        self.setStyleSheet(self.__unselectedStyleSheet)
+
+
+
+
+#########################################################
+## 
+## class for synchronizing all SymbolSelectorItems
+##
+#########################################################
+class Synchronizer(object):
+
+
+    def __init__(self):
+
+        self.__activeWidget = None
+
+
+    def select(self, target):
+
+        if self.__activeWidget == target:
+            self.__activeWidget.inactivate_me()
+            self.__activeWidget = None
+        else:
+            if self.__activeWidget:
+                self.__activeWidget.inactivate_me()
+
+            self.__activeWidget = target
+            self.__activeWidget.activate_me()
+
+        if self.__activeWidget:
+            print(self.__activeWidget.name)
 
 
