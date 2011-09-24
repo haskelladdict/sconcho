8c7197dff9b9bc139525d1f70dffedec9e975052
Markus Dittrich <haskelladdict@users.sourceforge.net>
2011-09-24 12:56:06 -0400

    Make sure PatternCanvas is a child of MainWindow
    Otherwise any non-modal dialogs of PatternCanvas such
    as ManageGridDialog won't die when we close MainWindow.

--- sconcho/gui/main_window.py
+++ sconcho/gui/main_window.py
@@ -83,7 +83,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self._topLevelPath = topLevelPath
         self._knittingSymbols = knittingSymbols
         self.canvas = PatternCanvas(self.settings, 
-                                    knittingSymbols[QString("knit")])
+                                    knittingSymbols[QString("knit")], self)
         self.initialize_symbol_widget(knittingSymbols)
         self.initialize_color_widget()
         self.initialize_row_col_widget()
