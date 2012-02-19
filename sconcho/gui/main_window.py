6817a96154bc77503bef0191ff75f3b9df8e8a3a
Markus Dittrich <haskelladdict@users.sourceforge.net>
2012-02-19 10:28:03 -0500

    Updated tentative release date.

--- sconcho/gui/main_window.py
+++ sconcho/gui/main_window.py
@@ -27,7 +27,7 @@ from __future__ import absolute_import
 
 # version and release date of current sconcho
 __version__ = "0.1.0_rc1"
-__releaseDate__ = "2012-02-14"
+__releaseDate__ = "2012-02-20"
 
 import logging
 import platform, os
