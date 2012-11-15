c0595cfb8f76520ce09861af17715e7ae24fce5a
Markus Dittrich <haskelladdict@users.sourceforge.net>
2012-11-15 10:03:25 -0500

    Update version info.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     dataFiles.append('doc/manual.html')
 
     setup(name='sconcho',
-        version='0.2.0_b3',
+        version='0.2.0_b4',
         description='Program for Generating Knitting Charts',
         author='Markus Dittrich',
         author_email='haskelladdict@users.sourceforge.net',
